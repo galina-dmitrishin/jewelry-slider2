e7b2ca7ed553dcdb63ded6e187b6017187722d9e
galina-dmitrishin <67236906+galina-dmitrishin@users.noreply.github.com>
2022-07-22 01:17:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.slider-1').slick({
+        //dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        //adaptiveHeight: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+  });
+
+
+  $(document).ready(function(){
+    $('.slider-2').slick({
+        //dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        //adaptiveHeight: true,
+        //fade: true,
+        //cssEase: 'linear'
+    });
+  });
+
+
+  $(document).ready(function(){
+    $('.slider-3').slick({
+        //dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        //adaptiveHeight: true,
+        //fade: true,
+        //cssEase: 'linear'
+    });
+  });
